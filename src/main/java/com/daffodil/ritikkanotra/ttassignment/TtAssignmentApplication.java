bb2575e48eaebc56e31bd110100429156ade438b
Ritik Kanotra <ritik.kanotra@unthinkable.co>
2021-11-30 15:27:43 +0530

    Rename: TtassignmentApplication.java -> tAssignmentApplication.java

--- src/main/java/com/daffodil/ritikkanotra/ttassignment/TtAssignmentApplication.java
+++ src/main/java/com/daffodil/ritikkanotra/ttassignment/TtAssignmentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TtassignmentApplication {
+public class TtAssignmentApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TtassignmentApplication.class, args);
+		SpringApplication.run(TtAssignmentApplication.class, args);
 	}
 
 }
