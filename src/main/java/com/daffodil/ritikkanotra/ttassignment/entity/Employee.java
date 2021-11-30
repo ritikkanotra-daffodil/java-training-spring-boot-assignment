404f4f255c4b45d82033717b7e9ccbe7890d8f65
Ritik Kanotra <ritik.kanotra@unthinkable.co>
2021-11-30 11:55:34 +0530

    add Employee entity, JPA, Lombok dependency

---
+++ src/main/java/com/daffodil/ritikkanotra/ttassignment/entity/Employee.java
@@ -0,0 +1,32 @@
+package com.daffodil.ritikkanotra.ttassignment.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long employeeId;
+    private String employeeCode;
+    private String employeeName;
+    private String employeeDesignation;
+    private String employeeDepartment;
+    private String employeeEmail;
+    // TODO Add Date employeeRegDate here
+
+
+
+}
