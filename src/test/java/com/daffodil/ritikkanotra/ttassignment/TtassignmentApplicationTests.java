a128ac7f339a920e3926efe6318b4dda11b61519
Ritik Kanotra <ritik.kanotra@unthinkable.co>
2021-11-30 11:07:17 +0530

    Add,Imported files created using Spring Initializer

---
+++ src/test/java/com/daffodil/ritikkanotra/ttassignment/TtassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daffodil.ritikkanotra.ttassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
