6d20b5ef7bda29d3773b8f9f3e574be84393c9d2
Ritik Kanotra <ritik.kanotra@unthinkable.co>
2021-11-30 12:08:04 +0530

    Add Department entity

---
+++ src/main/java/com/daffodil/ritikkanotra/ttassignment/entity/Department.java
@@ -0,0 +1,27 @@
+package com.daffodil.ritikkanotra.ttassignment.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long departmentId;
+    private String departmentCode;
+    private String departmentName;
+
+}
